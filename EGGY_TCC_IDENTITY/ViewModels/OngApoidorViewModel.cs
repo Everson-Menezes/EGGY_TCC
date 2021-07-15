698d834beee818d4aab292c447b1cee36a1674ec
Everson-Menezes <everson.menezes@fatec.sp.gov.br>
2021-07-15 10:51:14 -0300

    Iniciado apoiadores

---
+++ EGGY_TCC_IDENTITY/ViewModels/OngApoidorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EGGY_TCC_IDENTITY.ViewModels
+{
+    public class OngApoidorViewModel 
+    {
+        public int ID_ONG_APOIADOR { get; set; }
+        public int ID_APOIADOR { get; set; }        
+        public int ID_ONG { get; set; }
+        public ApoidorViewModel Apoidor { get; set; }
+        public OngViewModel Ong { get; set; }
+    }
+}
